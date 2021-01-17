cfc12ccc08353bb3f3af05ca1e89481b1ec9504b
Tanner Fiscus <tanner@tannerfiscus.com>
2021-01-16 21:51:30 -0500

    Prefix env variables with gatsby

--- src/components/firebase/Firebase.js
+++ src/components/firebase/Firebase.js
@@ -3,14 +3,14 @@ import 'firebase/auth';
 import 'firebase/database';
  
 const config = {
-  apiKey: process.env.NODE_ENV === 'production' ? JSON.parse(process.env.FIREBASE_API_KEY): process.env.FIREBASE_API_KEY,
-  authDomain: process.env.NODE_ENV === 'production' ? JSON.parse(process.env.FIREBASE_AUTH_DOMAIN): process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NODE_ENV === 'production' ? JSON.parse(process.env.FIREBASE_DATABASE_URL): process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.NODE_ENV === 'production' ? JSON.parse(process.env.FIREBASE_PROJECT_ID): process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NODE_ENV === 'production' ? JSON.parse(process.env.FIREBASE_STORAGE_BUCKET): process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NODE_ENV === 'production' ? JSON.parse(process.env.FIREBASE_MESSAGING_SENDER_ID): process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NODE_ENV === 'production' ? JSON.parse(process.env.FIREBASE_APP_ID): process.env.FIREBASE_APP_ID,
-  measurementId: process.env.NODE_ENV === 'production' ? JSON.parse(process.env.FIREBASE_MEASUREMENT_ID): process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.GATSBY_FIREBASE_API_KEY,
+  authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.GATSBY_FIREBASE_DATABASE_URL,
+  projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.GATSBY_FIREBASE_APP_ID,
+  measurementId: process.env.GATSBY_FIREBASE_MEASUREMENT_ID,
 };
  
 class Firebase {
