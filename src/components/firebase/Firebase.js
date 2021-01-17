eefeb3c9f1f8c9cbed99889c1469ccea5d6074f6
Tanner Fiscus <tanner@tannerfiscus.com>
2021-01-16 21:42:13 -0500

    Try to get env variables working with netlify

--- src/components/firebase/Firebase.js
+++ src/components/firebase/Firebase.js
@@ -3,14 +3,14 @@ import 'firebase/auth';
 import 'firebase/database';
  
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.NODE_ENV === 'production' ? JSON.parse(process.env.FIREBASE_API_KEY): process.env.FIREBASE_API_KEY,
+  authDomain: process.env.NODE_ENV === 'production' ? JSON.parse(process.env.FIREBASE_AUTH_DOMAIN): process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NODE_ENV === 'production' ? JSON.parse(process.env.FIREBASE_DATABASE_URL): process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.NODE_ENV === 'production' ? JSON.parse(process.env.FIREBASE_PROJECT_ID): process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NODE_ENV === 'production' ? JSON.parse(process.env.FIREBASE_STORAGE_BUCKET): process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NODE_ENV === 'production' ? JSON.parse(process.env.FIREBASE_MESSAGING_SENDER_ID): process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NODE_ENV === 'production' ? JSON.parse(process.env.FIREBASE_APP_ID): process.env.FIREBASE_APP_ID,
+  measurementId: process.env.NODE_ENV === 'production' ? JSON.parse(process.env.FIREBASE_MEASUREMENT_ID): process.env.FIREBASE_MEASUREMENT_ID,
 };
  
 class Firebase {
